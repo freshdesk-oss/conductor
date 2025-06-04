bdcc8368ed3c09f1e9369d355d291d25e9991ffd
akashvenkatesan0 <akash.venkatesan@freshworks.com>
2025-06-04 17:21:54 +0530

    redis lock enabled

--- core/src/main/java/com/netflix/conductor/service/ExecutionLockService.java
+++ core/src/main/java/com/netflix/conductor/service/ExecutionLockService.java
@@ -36,7 +36,7 @@ public class ExecutionLockService {
     private final long lockTimeToTry;
 
     @Autowired
-    public ExecutionLockService(ConductorProperties properties, @Qualifier("provideLock") Lock lock) {
+    public ExecutionLockService(ConductorProperties properties, @Qualifier("provideRedisLock") Lock lock) {
         this.properties = properties;
         this.lock = lock;
         this.lockLeaseTime = properties.getLockLeaseTime().toMillis();
